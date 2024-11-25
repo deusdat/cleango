79ce01c23886b824438a2b712a277e2045cf71b8
J Patrick Davenport <patrick.davenport@deusdatsolutions.com>
2024-11-25 06:54:51 -0500

    Enables a function use case approach

--- usecase.go
+++ usecase.go
@@ -40,3 +40,24 @@ func (w *WrappingUseCase[Input, Answer]) Execute(input Input, p Presenter[Answer
 	}()
 	w.Implementation.Execute(input, p)
 }
+
+// FunctionalUseCase is a generic implementation of UseCase that takes a function to act as the Execute method.
+type FunctionalUseCase[Input any, Answer any] struct {
+	ExecuteFunc func(Input) (Answer, error)
+}
+
+// Execute runs the provided function and passes its result to the presenter.
+func (f *FunctionalUseCase[Input, Answer]) Execute(input Input, p Presenter[Answer]) {
+	answer, err := f.ExecuteFunc(input)
+	p.Present(Output[Answer]{
+		Answer: answer,
+		Err:    err,
+	})
+}
+
+// NewFunctionalUseCase creates a new FunctionalUseCase with the provided function.
+func NewFunctionalUseCase[Input any, Answer any](executeFunc func(Input) (Answer, error)) *FunctionalUseCase[Input, Answer] {
+	return &FunctionalUseCase[Input, Answer]{
+		ExecuteFunc: executeFunc,
+	}
+}
